d5f0356442cbfa527fc7eb9ef18dfcaf57a709f9
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-17 18:30:25 -0500

    Se agrega el modulo aeropuerto

--- src/aerolinea/aerolinea.entity.ts
+++ src/aerolinea/aerolinea.entity.ts
@@ -1,1 +1,23 @@
-export class AerolineaEntity {}
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class AerolineaEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    descripcion: string;
+
+    @Column()
+    fechaFundacion: Date;
+
+    @Column()
+    paginaWeb: string;
+
+    @ManyToMany(() => CulturaGastronomicaEntity, culturaGastronomica => culturaGastronomica.restaurantes)
+    @JoinTable()
+    culturasGastronomicas: CulturaGastronomicaEntity[];
+}
