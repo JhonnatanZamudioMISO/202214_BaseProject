de4e1dc4330111955f3d7ec57d39b16cf3776a0b
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-17 18:30:38 -0500

    Se implementa el entitiy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AerolineaModule } from './aerolinea/aerolinea.module';
+import { AeropuertoModule } from './aeropuerto/aeropuerto.module';
 
 @Module({
-  imports: [AerolineaModule],
+  imports: [AerolineaModule, AeropuertoModule],
   controllers: [AppController],
   providers: [AppService],
 })
