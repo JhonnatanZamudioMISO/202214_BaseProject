9b0aeef617dfc5d3e9c3e21613a421cba1830b58
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 14:29:55 -0500

    Se implemento el método findOne

--- src/aeropuerto/aeropuerto.service.ts
+++ src/aeropuerto/aeropuerto.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
 import { Repository } from 'typeorm';
 import { AeropuertoEntity } from './aeropuerto.entity';
 
@@ -12,5 +13,12 @@ export class AeropuertoService {
 
     async findAll(): Promise<AeropuertoEntity[]> {
         return await this.aeropuertoRepository.find({ relations: ["aerolineas"] });
+    }
+
+    async findOne(id: string): Promise<AeropuertoEntity> {
+        const aeropuerto: AeropuertoEntity = await this.aeropuertoRepository.findOne({where: {id}, relations: ["aerolineas"] } );
+        if (!aeropuerto)
+          throw new BusinessLogicException("No se encontró el aeropuerto con la identificación proporcionada", BusinessError.NOT_FOUND);
+        return aeropuerto;
     }
 }
