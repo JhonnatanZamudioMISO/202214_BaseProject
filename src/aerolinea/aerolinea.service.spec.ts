6ebec2bcfe4d7575ad00c2b19f813c3041441d86
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 13:57:32 -0500

    se implemento prueba unitaria update deberia modificar una aerolinea

--- src/aerolinea/aerolinea.service.spec.ts
+++ src/aerolinea/aerolinea.service.spec.ts
@@ -89,5 +89,17 @@ describe('AerolineaService', () => {
     await expect(() => service.create(aerolinea)).rejects.toHaveProperty("message", "La fecha de fundación no puede ser superior a la fecha actual")
   });
 
+  it('update deberia modificar una aerolinea', async () => {
+    const aerolinea: AerolineaEntity = listaAerolineas[0];
+    aerolinea.nombre = "Nuevo nombre";
+    aerolinea.descripcion = "Nueva descripción";
+    const aerolineaActualizada: AerolineaEntity = await service.update(aerolinea.id, aerolinea);
+    expect(aerolineaActualizada).not.toBeNull();
+    const aerolineaGuardada: AerolineaEntity = await repository.findOne({ where: { id: aerolinea.id } })
+    expect(aerolineaGuardada).not.toBeNull();
+    expect(aerolineaGuardada.nombre).toEqual(aerolinea.nombre)
+    expect(aerolineaGuardada.descripcion).toEqual(aerolinea.descripcion)
+  });
+
 
 });
