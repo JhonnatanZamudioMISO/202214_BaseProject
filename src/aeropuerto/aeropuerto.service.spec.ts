4eeb54f3b726fd0439f70daf98a1f4c33859f71e
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 15:02:28 -0500

    Se configura correctamente la suite para pruebas unitarias

--- src/aeropuerto/aeropuerto.service.spec.ts
+++ src/aeropuerto/aeropuerto.service.spec.ts
@@ -1,16 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { AeropuertoEntity } from './aeropuerto.entity';
 import { AeropuertoService } from './aeropuerto.service';
+import { faker } from '@faker-js/faker';
 
 describe('AeropuertoService', () => {
   let service: AeropuertoService;
+  let repository: Repository<AeropuertoEntity>;
+  let listaAeropuertos: AeropuertoEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [AeropuertoService],
     }).compile();
-
     service = module.get<AeropuertoService>(AeropuertoService);
+    repository = module.get<Repository<AeropuertoEntity>>(getRepositoryToken(AeropuertoEntity));
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    listaAeropuertos = [];
+    for(let i = 0; i < 5; i++){
+        const aeropuerto: AeropuertoEntity = await repository.save({
+        nombre: faker.company.name(),
+        codigo: faker.lorem.word(3),
+        pais: faker.address.country(),
+        ciudad: faker.address.city()})
+        listaAeropuertos.push(aeropuerto);
+    }
+  }
 
   it('should be defined', () => {
     expect(service).toBeDefined();
