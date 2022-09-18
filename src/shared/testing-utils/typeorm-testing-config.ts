3ba7a81c6570a1510ba088f7e9a2f2a4cec3195a
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 12:20:48 -0500

    Se configura el archivo typeorm para las pruebas unitarias

---
+++ src/shared/testing-utils/typeorm-testing-config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AerolineaEntity } from "src/aerolinea/aerolinea.entity";
+import { AeropuertoEntity } from "src/aeropuerto/aeropuerto.entity";
+
+export const TypeOrmTestingConfig = () => [
+    TypeOrmModule.forRoot({
+        type: 'sqlite',
+        database: ':memory:',
+        dropSchema: true,
+        entities: [AerolineaEntity, AeropuertoEntity],
+        synchronize: true,
+        keepConnectionAlive: true
+    }),
+    TypeOrmModule.forFeature([AerolineaEntity, AeropuertoEntity]),
+];
