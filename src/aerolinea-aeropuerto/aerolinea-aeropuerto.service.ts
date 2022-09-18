5a3a468062c164055af4f5f33afcd059ed712965
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 15:57:00 -0500

    se configura correctamente el servicio

--- src/aerolinea-aeropuerto/aerolinea-aeropuerto.service.ts
+++ src/aerolinea-aeropuerto/aerolinea-aeropuerto.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AerolineaEntity } from 'src/aerolinea/aerolinea.entity';
+import { AeropuertoEntity } from 'src/aeropuerto/aeropuerto.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AerolineaAeropuertoService {}
+export class AerolineaAeropuertoService {
+    constructor(
+        @InjectRepository(AerolineaEntity)
+        private readonly aerolineaRepository: Repository<AerolineaEntity>,
+        @InjectRepository(AeropuertoEntity)
+        private readonly aeropuertoRepository: Repository<AeropuertoEntity>
+    ){}
+}
