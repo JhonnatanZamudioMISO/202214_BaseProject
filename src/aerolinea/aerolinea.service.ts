07457a54bdf05b2b53517940705e93f772d92fa2
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-17 20:03:34 -0500

    Se implementa el metodo create con la condicion de la fecha de fundación

--- src/aerolinea/aerolinea.service.ts
+++ src/aerolinea/aerolinea.service.ts
@@ -20,4 +20,11 @@ export class AerolineaService {
           throw new BusinessLogicException("No se encontró la aerolinea con la identificación proporcionada", BusinessError.NOT_FOUND);
         return aerolinea;
     }
+
+    async create(aerolinea: AerolineaEntity): Promise<AerolineaEntity> {
+        const fechaActual: Date = new Date();
+        if(aerolinea.fechaFundacion > fechaActual)
+            throw new BusinessLogicException("La fecha de fundación no puede ser superior a la fecha actual", BusinessError.NOT_FOUND);
+        return await this.aerolineaRepository.save(aerolinea);
+    }
 }
