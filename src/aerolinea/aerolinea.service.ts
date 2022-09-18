da9b18e22d71ac955c7c3ed5f20024ec327cbdaf
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-17 19:45:32 -0500

    Se configura el constructor del servicio

--- src/aerolinea/aerolinea.service.ts
+++ src/aerolinea/aerolinea.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AerolineaEntity } from './aerolinea.entity';
 
 @Injectable()
-export class AerolineaService {}
+export class AerolineaService {
+    constructor(
+            @InjectRepository(AerolineaEntity)
+            private readonly aerolineaRepository: Repository<AerolineaEntity>  
+        ){}
+}
