d25d81e1798e3fdf1758259ab5d4be2ea3bc4287
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 14:26:00 -0500

    Se configura el service correctamente

--- src/aeropuerto/aeropuerto.service.ts
+++ src/aeropuerto/aeropuerto.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AeropuertoEntity } from './aeropuerto.entity';
 
 @Injectable()
-export class AeropuertoService {}
+export class AeropuertoService {
+    constructor(
+        @InjectRepository(AeropuertoEntity)
+        private readonly aeropuertoRepository: Repository<AeropuertoEntity>
+    ){}
+}
