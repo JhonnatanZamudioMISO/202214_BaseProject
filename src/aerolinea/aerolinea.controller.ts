5e0980f14e49c072d775d89ca21e3a52dc79d138
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 20:00:54 -0500

    Se agrega el interceptor de errores

--- src/aerolinea/aerolinea.controller.ts
+++ src/aerolinea/aerolinea.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { AerolineaService } from './aerolinea.service';
 
+@UseInterceptors(BusinessErrorsInterceptor)
 @Controller('airlines')
 export class AerolineaController {
     constructor(private readonly aerolineaService: AerolineaService) {}
