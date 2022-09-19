24786f8b82b4f38ca20193e7b0ecf67d8143a9a6
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 20:37:31 -0500

    Se agrega el manejo de interceptores

--- src/aeropuerto/aeropuerto.controller.ts
+++ src/aeropuerto/aeropuerto.controller.ts
@@ -1,8 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { AeropuertoService } from './aeropuerto.service';
 
+@UseInterceptors(BusinessErrorsInterceptor)
 @Controller('airports')
 export class AeropuertoController {
     constructor(private readonly aeropuertoService: AeropuertoService) {}
-    
+
 }
