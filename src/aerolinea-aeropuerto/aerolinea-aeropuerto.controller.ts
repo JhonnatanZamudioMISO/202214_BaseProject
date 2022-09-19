c6291f7549e8151a1eb0f1ae372cf11ee20bb2e8
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 20:49:12 -0500

    Se configura el endpoint y el constructor

--- src/aerolinea-aeropuerto/aerolinea-aeropuerto.controller.ts
+++ src/aerolinea-aeropuerto/aerolinea-aeropuerto.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { AerolineaAeropuertoService } from './aerolinea-aeropuerto.service';
 
-@Controller('aerolinea-aeropuerto')
-export class AerolineaAeropuertoController {}
+@Controller('airlines')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class AerolineaAeropuertoController {
+    constructor(private readonly aerolineaAeropuertoService: AerolineaAeropuertoService){}
+    
+}
