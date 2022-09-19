6f5a7f5ad170fbf9308550e33f349e8da4a2a7bc
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 20:40:53 -0500

    Se agrega el método create

--- src/aeropuerto/aeropuerto.controller.ts
+++ src/aeropuerto/aeropuerto.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { AeropuertoDto } from './aeropuerto.dto';
+import { AeropuertoEntity } from './aeropuerto.entity';
 import { AeropuertoService } from './aeropuerto.service';
 
 @UseInterceptors(BusinessErrorsInterceptor)
@@ -15,5 +18,11 @@ export class AeropuertoController {
     @Get(':aeropuertoId')
     async findOne(@Param('aeropuertoId') aeropuertoId: string) {
         return await this.aeropuertoService.findOne(aeropuertoId);
+    }
+
+    @Post()
+    async create(@Body() aeropuertoDto: AeropuertoDto) {
+        const aeropuerto: AeropuertoEntity = plainToInstance(AeropuertoEntity, aeropuertoDto);
+        return await this.aeropuertoService.create(aeropuerto);
     }
 }
