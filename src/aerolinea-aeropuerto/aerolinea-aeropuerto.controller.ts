9d022626fa7085b9396e077b5ddad10c0648ad6e
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 21:12:19 -0500

    Se implementa el metodo updateAirportsFromAirline

--- src/aerolinea-aeropuerto/aerolinea-aeropuerto.controller.ts
+++ src/aerolinea-aeropuerto/aerolinea-aeropuerto.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Param, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { AeropuertoDto } from 'src/aeropuerto/aeropuerto.dto';
+import { AeropuertoEntity } from 'src/aeropuerto/aeropuerto.entity';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { AerolineaAeropuertoService } from './aerolinea-aeropuerto.service';
 
@@ -20,5 +23,11 @@ export class AerolineaAeropuertoController {
     @Get(':aerolineaId/airports/:aeropuertoId')
     async findAirportFromAirline(@Param('aerolineaId') aerolineaId: string, @Param('aeropuertoId') aeropuertoId: string){
         return await this.aerolineaAeropuertoService.findAirportFromAirline(aerolineaId, aeropuertoId);
+    }
+
+    @Put(':aerolineaId/airports')
+    async updateAirportsFromAirline(@Body() aeropuertosDto: AeropuertoDto[], @Param('aerolineaId') aerolineaId: string){
+        const aeropuertos = plainToInstance(AeropuertoEntity, aeropuertosDto)
+        return await this.aerolineaAeropuertoService.updateAirportsFromAirline(aerolineaId, aeropuertos);
     }
 }
