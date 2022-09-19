cfb8e7d35c629847540641f8c8f16ab3369680ae
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 21:05:02 -0500

    Se implemento el metodo findAirportFromAirline

--- src/aerolinea-aeropuerto/aerolinea-aeropuerto.controller.ts
+++ src/aerolinea-aeropuerto/aerolinea-aeropuerto.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Post, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, Post, UseInterceptors } from '@nestjs/common';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { AerolineaAeropuertoService } from './aerolinea-aeropuerto.service';
 
@@ -10,5 +10,10 @@ export class AerolineaAeropuertoController {
     @Post(':aerolineaId/airports/:aeropuertoId')
     async addAirportToAirline(@Param('aerolineaId') aerolineaId: string, @Param('aeropuertoId') aeropuertoId: string){
         return await this.aerolineaAeropuertoService.addAirportToAirline(aerolineaId, aeropuertoId);
+    }
+
+    @Get(':aerolineaId/airports/:aeropuertoId')
+    async findAirportFromAirline(@Param('aerolineaId') aerolineaId: string, @Param('aeropuertoId') aeropuertoId: string){
+        return await this.aerolineaAeropuertoService.findAirportFromAirline(aerolineaId, aeropuertoId);
     }
 }
