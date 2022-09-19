cb8aa39822946d513064d719494a87e47b514428
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2022-09-18 20:06:32 -0500

    Se configuro el DTO para aerolinea

--- src/aerolinea/aerolinea.dto.ts
+++ src/aerolinea/aerolinea.dto.ts
@@ -1,1 +1,20 @@
-export class AerolineaDto {}
+import { IsString } from "class-validator";
+
+export class AerolineaDto {
+
+    @IsString()
+    @IsNotEmpty()
+    readonly nombre: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly descripcion: string;
+
+    @IsDateString()
+    @IsNotEmpty()
+    readonly fechaFundacion: Date;
+
+    @IsUrl()
+    @IsNotEmpty()
+    readonly paginaWeb: string;
+}
